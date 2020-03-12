85669123ad9f02302cd8915ab190ed96295ea7e7
unknown <sebastian.icp@hotmail.com>
2020-03-12 15:14:49 -0300

    add loaders to webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
-const path = require('path')
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
+    contentBase: path.resolve(__dirname, "public")
+  },
+
+  //add loaders for vue/css/js files
+  module: {
+      //loaders
+    rules: [
+      { test: /\.vue$/, loader: "vue-loader" },
+      {
+        test: /\.(scss|css)$/,
+        loader: ["vue-style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.js$/, loader: "babel.loader", exclude: /node_modules/ }
+    ],
+    //easier install vue components
+    resolve: {
+      alias: {
+        vue$: "vue/dist/vue.esm.js"
+      },
+      extensions: ["*", ".js", ".vue", ".json"]
+    },
+    plugins: [VueLoaderPlugin()]
   }
-}
+};
